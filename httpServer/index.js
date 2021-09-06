6003b9250089188d7e6f0d42f17050a1ef9928d4
Mohit Mistry <mohitmistry1758@gmail.com>
2021-09-07 03:02:24 +0530

    19

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,13 +1,17 @@
+const fs = require('fs');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const data = fs.readFileSync(`${__dirname}/stuentData/stuentData.json`, "utf-8");
+    const datajs = JSON.parse(data);
     if (req.url == "/") {
         res.end("Welcome to home page :)");
 
     } else if (req.url == "/contect") {
         res.end("Welcome to contect page");
+    } else if (req.url == "/studentapi") {
+        res.end(`<h1>Welcome to student data</h1> ${datajs[0].name}`);
     } else {
-        res.writeHead(404);
         res.end("<h1> 404 Error </h1>");
     }
     // re.end("Hello this is mohit and welcome to listing port 8000 :)");
